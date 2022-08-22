40ffc0e1f3215539f47fb09f3f59c58086bcef36
John Deschamps-Wright <jdeschampswright@gmail.com>
2022-08-22 17:17:38 +0200

    navbar modified and validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :bikes
+  validates :username, presence: true
+  validates :email, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
