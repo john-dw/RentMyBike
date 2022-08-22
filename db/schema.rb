ca90e68c3b026c65370a389d252ea65ed9ffc6e0
Jeremy M <jm0033@tutamail.com>
2022-08-22 16:12:01 +0200

    Added column user_id as reference to table Bikes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_122441) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_140254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,8 +24,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_122441) do
     t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_bikes_on_user_id"
   end
-    
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_122441) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "bikes", "users"
 end
