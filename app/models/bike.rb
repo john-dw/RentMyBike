2ce8bb898bdf3b5bc074133c6dd5173d18c0ba03
Jeremy M <jm0033@tutamail.com>
2022-08-22 14:51:51 +0200

    added validates and belongs_to to Bike Model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,4 +1,11 @@
 class Bike < ApplicationRecord
+  belongs_to :user
+  has_many :bookings
 
-  
+  validates :category, presence: true
+  validates :price_per_day, presence: true
+  validates :location, presence: true
+  validates :name, presence: true
+  validates :model, presence: true
+  validates :picture_url, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'bikes/index'
-  get 'bikes/show'
-  get 'bikes/new'
-  get 'bikes/create'
-  get 'bikes/edit'
-  get 'bikes/update'
-  get 'bikes/destroy'
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources:bikes
 end
