09255a8b50277b4878e407fc82b94c3201a6ec08
John Deschamps-Wright <jdeschampswright@gmail.com>
2022-08-23 16:30:36 +0200

    total_price column in bookings and total_price computing in controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_094916) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_142536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_094916) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_price"
     t.index ["bike_id"], name: "index_bookings_on_bike_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
