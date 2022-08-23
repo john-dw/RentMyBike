0bede63c2d93b15dd0a9b6c908f0c022680a2e96
John Deschamps-Wright <jdeschampswright@gmail.com>
2022-08-23 12:32:53 +0200

    dashboard page and footer modification

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'bookings/accept'
   get 'bookings/reject'
+  get "dashboard", to: "pages#dashboard"
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
