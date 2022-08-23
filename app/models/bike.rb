c38ab88548a2e643aa5128cb5264737ef5708902
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2022-08-23 10:56:13 +0200

    Adding Category dropdown to Bike Model and creating partial for the form

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,7 +3,7 @@ class Bike < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
 
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: %w(Road Mountain City Gravel Touring) }
   validates :price_per_day, presence: true
   validates :location, presence: true
   validates :name, presence: true
